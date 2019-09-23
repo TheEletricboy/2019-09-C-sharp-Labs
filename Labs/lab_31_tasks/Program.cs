28684d0f650d0889f0f10a291612b2efde824db6
TheEletricboy <54644862+TheEletricboy@users.noreply.github.com>
2019-09-23 14:09:35 +0100

    monday push 2pm

---
+++ Labs/lab_31_tasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_31_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //anonymous (lambda) delegate
+            var task01 = new Task(() => { Console.WriteLine("running task01"); }   );
+            task01.Start();
+
+            //delegate is a PLACEHOLDER for one or more methods
+            //the simplest delegate is called an ACTION
+            //ACTION: void DoThis(){}     //no parameters in, void output
+            var taskOld = new Task(   DoThis  );
+            taskOld.Start();
+
+            var task02 = Task.Run(() => { Console.WriteLine("task02 is running"); });
+
+
+            Console.ReadLine();
+        }
+
+        static void DoThis()
+        {
+            Console.WriteLine("I am doing something");
+        }
+    }
+}
