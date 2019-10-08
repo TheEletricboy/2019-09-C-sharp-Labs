660a4c5fd9f4cba0a9ae78954e171019c08811aa
TheEletricboy <54644862+TheEletricboy@users.noreply.github.com>
2019-10-08 16:28:30 +0100

    tuesday push

---
+++ 20191008 Tuesday (APIs, reading data as json website).md
@@ -0,0 +1,75 @@
+##Tuesday 201910 APIs
+
+#APIs
+
+Goal for today:
+	
+	.NET Core
+
+		Build API from scratch with SQLite
+
+	Console App ==> read data as JSON
+
+	Website ==> read data as JSON and put on site
+
+What is an API
+	
+	Application Programming Interface
+
+			Method for transfering web data across the internet
+
+				PLAIN TEXT
+
+				JSON { "FIELD":"VALUE"}
+
+				XML <root><element field="value"></element></root>
+
+			Yesterday we looked at SERIALIZING DATA TO XML AND BACK
+
+			Today we are going to look at similar but instead of just saving a FILE as JSON
+			we are going to EXPOSE ENDPOUNTS IN HTTP where a client may OBTAIN DATA
+
+			GET 		/customers/ 	all
+			GET 		/customers/10 	one with id
+
+																GET ==> Data sent in URL visible to all
+
+ 			POST 		/customers/10 	update
+ 			POST 		/customers/10 	delete
+
+ 			DELETE
+ 			PUT(update) but depends on application: basic HTML they dont have PUT/DELETE
+
+#Let's start with a simple MODEL pulling in NORTHWIND and using SCAFFOLDING to build API
+
+	install-package microsoft.entityframeworkcore -v 2.1.1
+												 .sqlite
+												 .sqlserver
+												 .design
+
+ 	and up top of the cs file we put: using Microsoft.EntityFrameworkCore;
+
+
+#Consuming an API
+
+	We are building a console app to consume or use our api
+
+	install-package newtonsoft.json
+
+	Getting data from the internet
+
+		The model in use is to get data ASYNCHRONOUSLY ie wait for it.
+
+			Main Thread ==> ASYNC CALL generates a SUB-THREAD
+													ie: as TASK
+
+			Tasks: 2 types
+				1)returns VOID
+				2)returns Task<t> and get data we use myTask.result
+
+			Also tasks returning a <t> must be declared in an Async method where we await 
+			the web call
+
+				async Task<t> MyMethod
+
+					
---
+++ Labs/lab_46_mvc_consume_api/Controllers/ProductsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using lab_46_mvc_consume_api.Models;
+using System.Net.Http;
+using Newtonsoft;
+
+namespace lab_46_mvc_consume_api.Controllers
+{
+    public class ProductsController : Controller
+    {
+        static List<Product> products = new List<Product>();
+        static string url = "https://localhost:44341/api/products";
+        // GET: Products
+        public ActionResult Index()
+        {
+            using (var client = new HttpClient())
+            {
+                var productAsJSONString = client.GetStringAsync(url);
+                products = JsonConvert.DeserializeObject<List<Product>>(productAsJSONString.Result);
+            }
+
+            
+            return View();
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
